7d89214f21c98395d83cfcaa5f304bd63524c856
matthewsidneyjacobs <matthewsidneyjacobs@gmail.com>
2016-11-10 23:33:48 -0700

    got layout of how cards should appear on screen

---
+++ script.js
@@ -0,0 +1,17 @@
+$(document).ready(function() {
+  $('#card1').on('click', function(){
+    $(this).css("background-image",''));
+  });
+});
+
+
+//arrays to represent the photos. randomize the order of the array to randomize the order of photos.
+//when you click on the photo it is flipped over to reveal photo
+//click on two photos, if they match (maybe if they match photo id) then they cannot be unflipped.
+//need a counter to show how many guesses you have made
+//want to be able to  have a drop down menu to pick a bonobo to find
+
+
+//is it better to have 3 games? by that i mean have three sets of images as opposed to randomly assigning images to div spots each game?
+
+//maybe an array of objects. each object has an id and a src code?
